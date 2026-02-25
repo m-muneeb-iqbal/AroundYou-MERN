9c79458e226f0d90f252aee49d9f937ffd662adf
m-muneeb-iqbal <muneebiq24@gmail.com>
2026-02-25 23:22:04 +0500

    Login Controller wasnt working after the addition of upload picture. Now fine

--- server/src/index.js
+++ server/src/index.js
@@ -13,7 +13,8 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true })); 
 app.use(cookieParser());
 app.use(
     cors({
