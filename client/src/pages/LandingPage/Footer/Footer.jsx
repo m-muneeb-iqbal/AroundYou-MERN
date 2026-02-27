6091f8333a62c5f1fb31afed452fbd160c4d8457
m-muneeb-iqbal <muneebiq24@gmail.com>
2026-02-27 06:00:59 +0500

    Improved readability of LandingPage Footer.jsx and introduced grid system from react-bootstrap (gonna do it for every file now)

--- client/src/pages/LandingPage/Footer/Footer.jsx
+++ client/src/pages/LandingPage/Footer/Footer.jsx
@@ -1,139 +1,168 @@
+import { Container, Row, Col } from "react-bootstrap";
+
 import styles from "../../../styles/LandingPage/Footer/Footer.module.css";
 
 const Footer = () => {
-  return (
-    <footer>
-      <div className="container mt-0">
-        <div className={`row ${styles.footerSection1}`}>
-          <div className="col-md-4 col-12 d-flex justify-content-md-center">
-            <p className="h1">About</p>
-          </div>
 
-          <div className="col-md-7 col-12 d-flex flex-column justify-content-end">
-            <p>
-              AROUNDYOU was created by Comsats Under-Graduate student who wanted to
-              create a AROUNDYOU community, a place to stay connected with their
-              university friends and segment university life onto one all-inclusive
-              platform.
-            </p>
+    return (
 
-            <p>
-              A private and encrypted social media application to share content,
-              posts, stories, search for new friends, encrypted chat and make it
-              easier to schedule events with our RSVP feature.
-            </p>
+        <footer>
 
-            <p>
-              AROUNDYOU was created for U, the university students, and alumni to stay
-              connected.
-            </p>
+            <Container className="mt-0">
 
-            <p>
-              AROUNDYOU provides 100% User control over content, by allowing you to
-              select who sees your content and allowing you to decide for how
-              long the content will live before being deleted permanently off
-              our servers.
-            </p>
+                <Row className={styles.footerSection1}>
 
-            <p>
-              We look forward to growing AROUNDYOU and adding new features and
-              products over the next few months and would love to hear your
-              feedback, so please reach out to us.
-            </p>
+                    <Col xs={12} md={4} className="d-flex justify-content-md-center">
+                    
+                        <p className="h1">About</p>
 
-            <p>Welcome to our AROUNDYOU community,</p>
+                    </Col>
 
-            <p className="fw-bold">TEAM AROUNDYOU</p>
-          </div>
-        </div>
-      </div>
+                    <Col xs={12} md={7} className="d-flex flex-column justify-content-end">
+                    
+                        <p>
+                            AROUNDYOU was created by Comsats Under-Graduate student who wanted to
+                            create a AROUNDYOU community, a place to stay connected with their
+                            university friends and segment university life onto one all-inclusive
+                            platform.
+                        </p>
 
-      <div className={`container-fluid px-5 ${styles.footerSection2}`}>
-        <div className="row">
-          <div className="col-lg-10 col-sm-8 col-12 d-flex flex-column">
-            <div className="col-lg-10 col-sm-8 col-12 d-flex justify-content-sm-start justify-content-center mt-3">
-              <img
-                src="../Images/notIcons/edyou-footer.svg"
-                alt="aroundyou logo"
-                className="img-fluid"
-                height="100px"
-              />
-            </div>
+                        <p>
+                            A private and encrypted social media application to share content,
+                            posts, stories, search for new friends, encrypted chat and make it
+                            easier to schedule events with our RSVP feature.
+                        </p>
 
-            <div className="col-lg-10 col-sm-8 col-12 d-flex justify-content-sm-start justify-content-center mt-3">
-              <div>College Life. Stay Connected</div>
-            </div>
+                        <p>
+                            AROUNDYOU was created for U, the university students, and alumni to stay
+                            connected.
+                        </p>
 
-            <div className="col-lg-10 col-sm-8 col-12 d-flex justify-content-sm-start justify-content-center mt-sm-3 mb-sm-5 mt-3 mb-1">
-              <img
-                src="../Images/notIcons/apple-button-footer.svg"
-                alt="app store download button"
-                className={`img-fluid ${styles.footerAppleButton}`}
-                height="35px"
-              />
-            </div>
-          </div>
+                        <p>
+                            AROUNDYOU provides 100% User control over content, by allowing you to
+                            select who sees your content and allowing you to decide for how
+                            long the content will live before being deleted permanently off
+                            our servers.
+                        </p>
 
-          <div className="col-lg-2 col-sm-4 col-12 d-flex justify-content-sm-start justify-content-center">
-            <ul className={`${styles.ul} nav flex-column align-items-sm-start align-items-center mt-sm-3 mt-1`}>
-              <li className="nav-item">
-                <a className={`nav-link ${styles.footerItems}`} href="#">
-                  Features
-                </a>
-              </li>
+                        <p>
+                            We look forward to growing AROUNDYOU and adding new features and
+                            products over the next few months and would love to hear your
+                            feedback, so please reach out to us.
+                        </p>
 
-              <li className="nav-item">
-                <a className={`nav-link ${styles.footerItems}`} href="#">
-                  About Us
-                </a>
-              </li>
+                        <p>Welcome to our AROUNDYOU community,</p>
+                        <p className="fw-bold">TEAM AROUNDYOU</p>
 
-              <li className="nav-item">
-                <a className={`nav-link ${styles.footerItems}`} href="#">
-                  Schools
-                </a>
-              </li>
+                    </Col>
 
-              <li className="nav-item">
-                <a className={`nav-link ${styles.footerItems}`} href="#">
-                  Wishlist
-                </a>
-              </li>
+                </Row>
 
-              <li className="nav-item">
-                <a className={`nav-link ${styles.footerItems}`} href="#">
-                  Contact Us
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+            </Container>
 
-        <div className="row">
-          <div className="col-lg-9 col-12 d-flex justify-content-lg-start justify-content-center">
-            <p className={styles.noHoverPrivacyPolicy}>
-              All rights reserved. AroundYOU © 2026
-            </p>
-          </div>
+            <Container fluid="xs" className={`px-5 ${styles.footerSection2}`}>
 
-          <div className="col-lg-3 col-12 d-flex justify-content-center">
-            <a href="#" className={styles.footerItems}>
-              Privacy Policy
-            </a>
+                <Row>
 
-            <a href="#" className={styles.footerItems}>
-              <img
-                src="../Images/icons/Globe.png"
-                className={styles.footerItems}
-                alt="globe"
-              />
-              English
-            </a>
-          </div>
-        </div>
-      </div>
-    </footer>
-  );
+                    <Col xs={12} sm={8} lg={10} className="d-flex flex-column">
+                    
+                        <Col xs={12} sm={8} lg={10} className="d-flex justify-content-sm-start justify-content-center mt-3">
+                        
+                            <img src="../Images/notIcons/edyou-footer.svg" alt="aroundyou logo" className="img-fluid" height="100px"/>
+                        
+                        </Col>
+
+                        <Col xs={12} sm={8} lg={10} className="d-flex justify-content-sm-start justify-content-center mt-3">
+                        
+                            <div>College Life. Stay Connected</div>
+                        
+                        </Col>
+
+                        <Col xs={12} sm={8} lg={10} className="d-flex justify-content-sm-start justify-content-center mt-sm-3 mb-sm-5 mt-3 mb-1">
+                        
+                            <img src="../Images/notIcons/apple-button-footer.svg" alt="app store download button" className={`img-fluid ${styles.footerAppleButton}`} height="35px"/>
+                        
+                        </Col>
+                    
+                    </Col>
+
+                    <Col xs={12} sm={4} lg={2} className="d-flex justify-content-sm-start justify-content-center">
+                    
+                        <ul className={`${styles.ul} nav flex-column align-items-sm-start align-items-center mt-sm-3 mt-1`}>
+
+                            <li className="nav-item">
+
+                                <a className={`nav-link ${styles.footerItems}`} href="#">
+                                    Features
+                                </a>
+
+                            </li>
+                        
+                            <li className="nav-item">
+
+                                <a className={`nav-link ${styles.footerItems}`} href="#">
+                                    About Us
+                                </a>
+
+                            </li>
+                        
+                            <li className="nav-item">
+
+                                <a className={`nav-link ${styles.footerItems}`} href="#">
+                                    Schools
+                                </a>
+
+                            </li>
+                        
+                            <li className="nav-item ">
+
+                                <a className={`nav-link ${styles.footerItems}`} href="#">
+                                    Wishlist
+                                </a>
+
+                            </li>
+                        
+                            <li className="nav-item">
+
+                                <a className={`nav-link ${styles.footerItems}`} href="#">
+                                    Contact Us
+                                </a>
+
+                            </li>
+
+                        </ul>
+                    
+                    </Col>
+
+                </Row>
+
+                <Row>
+
+                    <Col xs={12} lg={9} className="d-flex justify-content-lg-start justify-content-center">
+
+                        <p className={styles.noHoverPrivacyPolicy}>All rights reserved. AroundYOU © 2026</p>
+                    
+                    </Col>
+
+                    <Col xs={12} lg={3} className="d-flex justify-content-center">
+
+                        <a href="#" className={styles.footerItems}>Privacy Policy</a>
+                        
+                        <a href="#" className={styles.footerItems}>
+                            <img src="../Images/icons/Globe.png" className={styles.footerItems} alt="globe"/>
+                            English
+                        </a>
+                    
+                    </Col>
+
+                </Row>
+
+            </Container>
+
+        </footer>
+
+    );
+
 };
 
 export default Footer;
