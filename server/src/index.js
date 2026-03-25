841f106583aa57af1be5b448a99960e5118dc764
m-muneeb-iqbal <muneebiq24@gmail.com>
2026-03-25 10:51:23 +0500

    still working on CORS

--- server/src/index.js
+++ server/src/index.js
@@ -21,7 +21,6 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true })); 
 app.use(cookieParser());
 app.use(
@@ -34,6 +33,12 @@ app.use(
     })
 );
 
+app.options("*", cors({
+    origin: "https://aroundyou-one.vercel.app",
+    credentials: true
+}));
+
+app.use(express.json({ limit: "10mb" }));
 
 app.use(passport.initialize());
 app.use("/api/auth", authRoutes);
