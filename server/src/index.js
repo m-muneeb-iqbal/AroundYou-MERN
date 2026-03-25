c8150632296e77634acd75c1786d0ff479d6f17e
m-muneeb-iqbal <muneebiq24@gmail.com>
2026-03-25 10:54:47 +0500

    rollback 2

--- server/src/index.js
+++ server/src/index.js
@@ -30,6 +30,7 @@ app.use(
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
+        optionsSuccessStatus: 200
     })
 );
 
